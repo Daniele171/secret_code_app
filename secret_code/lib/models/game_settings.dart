7dc50acfc15205fb3fb2b04803e63d036fee64aa
Daniele171 <daniele@users.noreply.github.com>
2026-01-01 21:50:51 +0100

    Refactor comments in GameSettings class for clarity

--- secret_code/lib/models/game_settings.dart
+++ secret_code/lib/models/game_settings.dart
@@ -2,12 +2,12 @@ class GameSettings {
   int codeLength;
   int maxRows;
   bool allowDuplicates;
-  int numberOfColors; // Numero di colori disponibili (8 = base, 12 = tutti)
+  int numberOfColors;
 
   GameSettings({
     this.codeLength = 4, 
     this.maxRows = 10,
     this.allowDuplicates = true,
-    this.numberOfColors = 4, // Default: solo i colori base
+    this.numberOfColors = 4,
   });
 }
